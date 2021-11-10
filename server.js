c5198901de4b437f030b572cc8dfdecea88c6c48
Gustavo Garzon <87545748+TavoGarzon@users.noreply.github.com>
2021-11-10 10:41:29 -0500

    despliegue

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ const onListening = () => {
   const bind = typeof port === "string" ? "pipe " + port : "port " + port;
 };
 
-const port = normalizePort(process.env.PORT || "https://git.heroku.com/blooming-scrubland-15594.git");
+const port = normalizePort(process.env.PORT || "blooming-scrubland-15594");
 app.set("port", port);
 
 const server = http.createServer(app);
