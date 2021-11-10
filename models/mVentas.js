1a47939c32c7dbb9cc0f0e1e6d2c6d86234ecf8b
Gustavo Garzon <87545748+TavoGarzon@users.noreply.github.com>
2021-11-10 10:04:30 -0500

    Initial commit

---
+++ models/mVentas.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+var uniqueValidator = require("mongoose-unique-validator");
+
+const venta = mongoose.Schema({
+  Fecha_Venta: { type: String, required: true },
+  Producto: { 
+    type:mongoose.Schema.Types.ObjectId, 
+    ref: "productos",
+    required: true, 
+  },
+  Referencia: { type: Number, required: true },
+  Precio: { type: Number, required: true },
+  Descripcion: { type: String },
+  Sucursal: { type: String, required: true },
+  Vendedor: { type: String, required: true },
+  Categoria: { type: String, required: true },
+});
+
+venta.plugin(uniqueValidator);
+
+module.exports = mongoose.model("Venta", venta);
